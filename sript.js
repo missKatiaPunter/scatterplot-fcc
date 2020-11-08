df10d8c97e3244fbfc6d33b8ac6743ce25d87b2c
Katia <katia.punter@storyshareplatform.com>
2020-11-08 20:27:42 +0000

    Gets data

--- sript.js
+++ sript.js
@@ -1,1 +1,12 @@
-console.log(d3);
+const dataSource = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
+let xScale;
+let yScale;
+
+let width = 800;
+let height = 600;
+let padding = 50;
+
+fetch(dataSource)
+    .then(res => res.json())
+    .then(res => console.log(res));
