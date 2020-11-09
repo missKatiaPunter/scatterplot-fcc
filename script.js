8ed861e3b04773629a1ef9929e13e213b9c85d14
Katia <katia.punter@storyshareplatform.com>
2020-11-09 16:15:19 +0000

    Adds scales and class dot to pass 1 more test

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-console.log("hello");
 const dataSource = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
 
 let xScale;
 let yScale;
 
-let width = 800;
-let height = 600;
-let padding = 50;
+const w = 800;
+const h = 600;
+const padding = 50;
+
+const r = 5; //sets the global for the circle radius
 
 fetch(dataSource)
-    .then(res => res.json())
-    .then(res => console.log(res));
+    .then(res => {
+        if (!res.ok) {
+            throw new Error("HTTP status " + res.status);
+        }
+            return res.json();
+        })
+    .then(data => {
+        let xExtent = d3.extent(data, d => d.Year);
+        let yExtent = d3.extent(data, d => d.Time)
+        xScale = d3.scaleLinear().domain(xExtent).range([padding, w-padding]);
+        yScale = d3.scaleTime().domain(yExtent).range([h-padding, padding]);
+        svg.selectAll("circle")
+        .data(data) //binds the data to non-existent yet circles
+        .enter() //creates one circle element for every datum in the array
+        .append("circle") //appends actual svg circles
+        .attr('class', 'dot') // passes dot class test
+        .attr('cx', d => xScale(d.Year))
+        .attr('cy', d => yScale(d.Time))
+        .attr('r', d=> r)
+        .attr('fill', 'yellow')
+
+    });
+
+const drawAxis = () => {
+
+}
+
+const svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+const toolTip = d3.select("body")
+    .append("div")
+
+
